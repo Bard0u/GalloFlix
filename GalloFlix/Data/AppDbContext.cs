51d673cab9819e736f3daddd2b9125b3b67e30c2
Bard0u <pedroarossettoo@gmail.com>
2023-05-02 12:28:13 -0300

    Finalização da classe de contexto

--- GalloFlix/Data/AppDbContext.cs
+++ GalloFlix/Data/AppDbContext.cs
@@ -9,43 +9,99 @@ public class AppDbContext : IdentityDbContext
 {
 
     public AppDbContext(DbContextOptions options) : base(options)
-    {   
+    {
     }
-    public DbSet<AppUser> AppUser {get; set;}
-    public DbSet<Genre> Genres {get; set;}
-    public DbSet<Movie> Movies {get; set;}
-    public DbSet<MovieComment> MovieComments {get; set;}
-    
-    public DbSet<MovieGenre> MovieGenres {get; set;}
-    
-    public DbSet<MovieRating> MovieRatings {get; set;}
+    public DbSet<AppUser> AppUser { get; set; }
+    public DbSet<Genre> Genres { get; set; }
+    public DbSet<Movie> Movies { get; set; }
+    public DbSet<MovieComment> MovieComments { get; set; }
+
+    public DbSet<MovieGenre> MovieGenres { get; set; }
+
+    public DbSet<MovieRating> MovieRatings { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-
-        builder.Entity<IdentityUser>(b => {
+        #region personalização do Identity
+        builder.Entity<IdentityUser>(b =>
+        {
             b.ToTable("Users");
         });
-        builder.Entity<IdentityUserClaim<string>>(b => {
+        builder.Entity<IdentityUserClaim<string>>(b =>
+        {
             b.ToTable("UserClaims");
         });
-        builder.Entity<IdentityUserLogin<string>>(b => {
+        builder.Entity<IdentityUserLogin<string>>(b =>
+        {
             b.ToTable("UserLogins");
         });
-        builder.Entity<IdentityUserToken<string>>(b => {
+        builder.Entity<IdentityUserToken<string>>(b =>
+        {
             b.ToTable("UserTokens");
         });
-        builder.Entity<IdentityRole>(b => {
+        builder.Entity<IdentityRole>(b =>
+        {
             b.ToTable("Roles");
         });
-        builder.Entity<IdentityRoleClaim<string>>(b => {
+        builder.Entity<IdentityRoleClaim<string>>(b =>
+        {
             b.ToTable("RolesClaims");
         });
-        builder.Entity<IdentityUserRole<string>>(b => {
+        builder.Entity<IdentityUserRole<string>>(b =>
+        {
             b.ToTable("UserRoles");
         });
+        #endregion
 
+        #region many to many - MovieContent
+
+        builder.Entity<MovieComment>()
+            .HasOne(mc => mc.Movie)
+            .WithMany(m => m.Comments)
+            .HasForeignKey(mc => mc.MovieId);
+
+        builder.Entity<MovieComment>()
+            .HasOne(mc => mc.User)
+            .WithMany(u => u.Comments)
+            .HasForeignKey(mc => mc.UserId);
+
+        #endregion
+
+        #region  Many to Many- MovieGenre
+        // Definição de Chave Primaria Composta
+        builder.Entity<MovieGenre>().HasKey(
+            mg => new { mg.MovieId, mg.GenreId }
+        );
+
+        builder.Entity<MovieGenre>()
+        .HasOne(mg => mg.Movie)
+        .WithMany(m => m.Genres)
+        .HasForeignKey(mg => mg.MovieId);
+
+        builder.Entity<MovieGenre>()
+        .HasOne(mg => mg.Genre)
+        .WithMany(global => global.Movies)
+        .HasForeignKey(mg => mg.GenreId);
+
+        #endregion
+
+        #region Many to Many - MovieRating
+        builder.Entity<MovieRating>().HasKey(
+            mr => new { mr.MovieId, mr.UserId }
+        );
+
+        builder.Entity<MovieRating>()
+            .HasOne(mr=> mr.Movie)
+            .WithMany(m => m.Ratings)
+            .HasForeignKey(mr => mr.MovieId);
+
+        builder.Entity<MovieRating>()
+        .HasOne(mr => mr.User)
+        .WithMany(global => global.Ratings)
+        .HasForeignKey(mr => mr.UserId);
+
+        #endregion
     }
 
 }
